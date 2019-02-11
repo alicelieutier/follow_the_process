a9e410e90cd42e80b5f2b5d5db112f1a8a9fa5ea
Alice Lieutier <alice@lieutier.me>
2019-02-11 16:11:31 +0000

    creating basic step component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Step from './components/Step';
+
 class App extends Component {
   render() {
+    const stubStep = {
+      title: 'Describe the problem',
+      description: 'Write down words that relate to the problem.',
+    }
     return (
       <div className="App">
-        
+        <Step {...stubStep} />
       </div>
     );
   }
---
+++ src/components/Step.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Step.css';
+
+const Step = ({title, description}) => (
+    <div className="Step">
+    <h1>{title}</h1>
+    <p>{description}</p>
+    </div>
+)
+
+export default Step;
